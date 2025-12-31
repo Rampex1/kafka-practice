d004a84e87bc7f1f9ced80d16cb3a563ec17eae4
David Zhou <david.zhou.0110@gmail.com>
2025-12-31 18:46:23 -0500

    Producer

---
+++ producer.py
@@ -0,0 +1,28 @@
+from confluent_kafka import Producer
+import json
+import uuid
+
+producer_config = {"bootstrap.servers": "localhost:9092"}
+
+producer = Producer(producer_config)
+
+
+def delivery_report(err, msg):
+    if err:
+        print(f"❌ Delivery failed: {err}")
+    else:
+        print(f"✅ Delivered: {msg.value().decode('utf-8')}")
+
+
+order = {
+    "order_id": str(uuid.uuid4()),
+    "user": "nana",
+    "item": "mushroom pizza",
+    "quantity": 2,
+}
+
+value = json.dumps(order).encode("utf-8")
+
+producer.produce(topic="orders", value=value, callback=delivery_report)
+
+producer.flush()
